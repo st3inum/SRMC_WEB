4d123bbb7d810ffb76a077e4a97f7391210a46fe
st3inum <st3inum@gmail.com>
2020-01-19 06:49:19 +0600

    navigation bar

--- config/constant.php
+++ config/constant.php
@@ -5,8 +5,7 @@ define('DB_PASS','');
 define('DB_NAME','SRMC.db');
 
 
-define('MYEMAIL','rakin.ahmed365@gmail.com');
-define('MYEMAILPASS','1@3$5^7*ahmed');
+
 // define('MYEMAIL','your email address');
 // define('MYEMAILPASS','password of your email');
 
