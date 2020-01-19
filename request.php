ff6d77dca0721a72eeab639ebc0f010e4348a613
st3inum <st3inum@gmail.com>
2020-01-19 12:10:42 +0600

    requests page backend for problem setter requests

--- request.php
+++ request.php
@@ -29,7 +29,43 @@
 
         <div class="hero-body ">
 	        	<div class="container has-text-centered">
-				      	This is Request page
+				      	<!-- This is Request page -->
+				      	<?php 
+				      		$sql="SELECT * FROM setterRequest";
+				      		$result=mysqli_query($conn,$sql);
+				      		if(mysqli_num_rows($result)>0)
+				      		{
+				      			while($row=mysqli_fetch_assoc($result)){
+				      				$userDetailsQuery="SELECT * FROM userInformation WHERE userNumber=".$row['userNumber'];
+				      				$res=mysqli_query($conn,$userDetailsQuery);
+				      				if(mysqli_num_rows($res)>0)
+				      				{
+				      					$user=mysqli_fetch_assoc($res);
+				      				}
+				      				else
+				      				{
+				      					header('location: errors.php');
+												exit();
+				      				}
+				      				?>
+				      				<!-- user description -->
+				      				<h1><?php echo $user['name']; ?></h1>
+				      				<p>hi please accept my request</p>
+				      				<p>
+				      					<button href="#">Accept</button>
+				      					<button href="#">Regect</button>
+				      				</p>
+				      				<!-- end user description -->
+
+
+				      			<?php
+				      			}
+				      		}else
+				      		{
+				      			echo "no pending request";
+				      		}
+
+				      	?>
 						</div>
         </div>
     </section>
