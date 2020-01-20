e7393428e27238558a5b709f962b99d0318cf244
st3inum <st3inum@gmail.com>
2020-01-21 03:49:39 +0600

    new fix

--- config/constant.php
+++ config/constant.php
@@ -10,10 +10,10 @@ define('DB_PASS','');
 
 define('DB_NAME','SRMC.db');
 
-
-
+define('VERIFYLOC','/problems/controllers/verify.php?token=');
 // define('MYEMAIL','your email address');
 
 // define('MYEMAILPASS','password of your email');
 
 ?>
+
